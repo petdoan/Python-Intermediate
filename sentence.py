01b49dfbfaa52cc9bbc04b9d97148b0a372fbaf4
Dave Wade-Stein <dws@DWS-MacBook.local>
2016-02-08 22:50:04 -0500

    adding sentence.py

--- sentence.py
+++ sentence.py
@@ -1,17 +1,18 @@
-import re
-
-RE_WORD = re.compile('\w+')
-
-class Sentence:
+class Sentence():
     def __init__(self, text):
         self.text = text
-        self.words = RE_WORD.findall(text) 
+        self.words = text.split()
+
+    def __repr__(self):
+        return repr(self.text)
 
     def __getitem__(self, index):
         return self.words[index] 
 
-'''
     def __len__(self): 
         return len(self.words)
 
-'''
+    def __add__(self, other):
+        '''Allow one Sentence to be added to another.'''
+        return Sentence(self.text + ' ' + other.text)
+
